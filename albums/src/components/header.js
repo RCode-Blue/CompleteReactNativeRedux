e6854821030588f1a7c89789215bc5ccf74f76e5
RCode-Blue <rickyliew@hotmail.com>
2018-08-29 17:22:06 +1000

    Completed Section 5

---
+++ albums/src/components/header.js
@@ -0,0 +1,11 @@
+// Import libraries
+import React from 'react';
+import { Text } from 'react-native';
+
+// Create component
+const Header = () => {
+  return <Text>Albums</Text>;
+};
+
+// Make component available
+export default Header;
