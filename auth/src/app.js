1a6e0e1ed8d6dd978ca7fb582838ffd62be6a707
RCode-Blue <rickyliew@hotmail.com>
2018-08-31 11:42:47 +1000

    9-59: Firebase Setup

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,8 +1,20 @@
+import firebase from 'firebase';
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
 
 class App extends Component{
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: '',
+      authDomain: 'rl-tut-reactnative-auth.firebaseapp.com',
+      databaseURL: 'https://rl-tut-reactnative-auth.firebaseio.com',
+      projectId: 'rl-tut-reactnative-auth',
+      storageBucket: 'rl-tut-reactnative-auth.appspot.com',
+      messagingSenderId: '967569997516'
+    });
+  }
+
   render(){
     return(
       <View>
